ebc3600944622e7805ca1356b4ad2ea0dc192f82
mhgutierrez <mhgutierrez85db@gmail.com>
2019-06-14 16:05:18 +0200

    Solución con clase

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php
 
-//controlador
+Class Bombilla{
+	function on(){
+	return '<a href="index.php?off=1"><img style="background:rgba(252, 245, 239,0.25); border-radius:100px; margin:50px 0 0 50px; padding:80px;border:1px solid #313D47" src="lightbulb_on.png"></a></div>';
+}
 
-if (isset($_GET['on'])){
-	echo on();	
+	function off(){
+	return '<a href="index.php?on=1"><img style="background:rgba(252, 245, 239,0.25); border-radius:100px; margin:50px 0 0 50px; padding:80px;border:1px solid #313D47" src="lightbulb_off.png"></a></div>';
+}
+	
+}
+
+//controlador
+$bombilla1 = new Bombilla();
+
+
+
+
+if (!isset($_GET['off'])){
+	echo $bombilla1->on();	
 	}
 	else{
-	echo off();
+		echo $bombilla1->off();
 	}
 	
-function on(){
-	return '<a href="index.php?off=1" ><img src="lightbulb_on.png"></a>';
-}
+	
 
-function off(){
-	return '<a href="index.php?on=1" ><img src="lightbulb_off.png"></a>';
-}
+
+
+
 
 
 
